cc0b867bdce8602b505517d7a51359eac34f42b9
Velmer <barkaloff.m@gmail.com>
2025-06-16 17:28:55 +0300

    auth middleware. default pip management changed to poetry. deleted shit

---
+++ app/middlewares/auth.py
@@ -0,0 +1,32 @@
+from typing import Awaitable, Callable, Optional, cast
+from fastapi import HTTPException
+from starlette.middleware.base import BaseHTTPMiddleware
+from starlette.requests import Request
+from starlette.responses import Response
+from starlette import status
+
+from app.api.jwt import UserAuthManager
+
+
+class RequiredAuthMiddleware(BaseHTTPMiddleware):
+    async def dispatch(
+        self, request: Request, call_next: Callable[[Request], Awaitable[Response]]
+    ) -> Response:
+        auth = request.cookies.get("Authorization")
+        leave = HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="Unauthorized"
+        )
+
+        if not auth:
+            raise leave
+
+        payload = UserAuthManager().decode_token(auth[7:])
+
+        sub = cast(Optional[str], payload.get("sub") if payload else None)
+
+        if not (sub and sub.isdigit()):
+            raise leave
+
+        request.state.user_id = int(sub)
+
+        return await call_next(request)
