fb6824c53851537855cffdaea32b467dc3a45539
Dexter2038 <116676321+Dexter2038@users.noreply.github.com>
2025-08-08 00:40:50 +0300

    removed unnecessary and fixed register
    removed init data check and tech works check in frontend as now backend
    preciously handles it and now we don't need to every time fetch this.
    backend redirect client. fixed registration instead of extracting init
    data on client we'll send only init data itself.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,116 +6,81 @@ import Game from "./components/Game";
 import Lottery from "./components/Lottery";
 import Technical from "./components/Technical";
 import {
-  GamesPage,
-  BlackjackPage,
-  BlackjackBot,
-  BlackjackGame,
-  DicePage,
-  DiceBot,
-  DiceGame,
-  GuessGame,
-  MinesGame,
-  RouletteGame,
-  CrashGame
+    GamesPage,
+    BlackjackPage,
+    BlackjackBot,
+    BlackjackGame,
+    DicePage,
+    DiceBot,
+    DiceGame,
+    GuessGame,
+    MinesGame,
+    RouletteGame,
+    CrashGame,
 } from "./components/Games";
 import RegisterPage from "./components/Register";
 import Waiting from "./components/Waiting";
 import { useIsConnectionRestored } from "@tonconnect/ui-react";
 import "./App.css";
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import getLaunchParams from "./components/RetrieveLaunchParams";
-import axios from "axios";
-
-
-
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
-  const { initDataRaw, initData } = getLaunchParams();
-  const navigate = useNavigate();
+    const conRestored = useIsConnectionRestored();
 
-  useEffect(() => {
-    axios
-      .post("/api/initdata/check", {
-        initData: initDataRaw,
-        player_id: initData?.user?.id,
-      })
-      .then((response) => {
-        const data = response.data;
-        if (!data.ok) {
-          if (data.msg === "Пользователь незарегистрирован") {
-            toast.warning("Сначала зарегистрируйтесь!")
-            navigate('/reg');
-          } else {
-            navigate("/technical");
-          }
-        } else {
-          navigate('/referal')
-        }
-      })
-      .catch(() => {
+    if (!conRestored) {
         return (
-          <></>
+            <>
+                <Routes>
+                    <Route path="/referal" element={<Referal />} />
+                    <Route path="/balance" element={<Balance />} />
+                    <Route path="/lottery" element={<Lottery />} />
+                    <Route path="/game" element={<Game />} />
+                    <Route path="/games" element={<GamesPage />} />
+                    <Route path="/dice" element={<DicePage />} />
+                    <Route path="/dice_game" element={<DiceGame />} />
+                    <Route path="/dice_bot" element={<DiceBot />} />
+                    <Route path="/blackjack" element={<BlackjackPage />} />
+                    <Route path="/blackjack_game" element={<BlackjackGame />} />
+                    <Route path="/blackjack_bot" element={<BlackjackBot />} />
+                    <Route path="/mines" element={<MinesGame />} />
+                    <Route path="/crash" element={<CrashGame />} />
+                    <Route path="/roulette" element={<RouletteGame />} />
+                    <Route path="/guess" element={<GuessGame />} />
+                    <Route path="/reg" element={<RegisterPage />} />
+                    <Route path="/technical" element={<Technical />} />
+                    <Route path="*" element={<Waiting />} />
+                </Routes>
+                <ToastContainer />
+            </>
         );
-      });
-  }, []);
+    }
 
-  const conRestored = useIsConnectionRestored();
-  if (!conRestored) {
     return (
-      <>
-        <Routes>
-          <Route path="/referal" element={<Referal />} />
-          <Route path="/balance" element={<Balance />} />
-          <Route path="/lottery" element={<Lottery />} />
-          <Route path="/game" element={<Game />} />
-          <Route path="/games" element={<GamesPage />} />
-          <Route path="/dice" element={<DicePage />} />
-          <Route path="/dice_game" element={<DiceGame />} />
-          <Route path="/dice_bot" element={<DiceBot />} />
-          <Route path="/blackjack" element={<BlackjackPage />} />
-          <Route path="/blackjack_game" element={<BlackjackGame />} />
-          <Route path="/blackjack_bot" element={<BlackjackBot />} />
-          <Route path="/mines" element={<MinesGame />} />
-          <Route path="/crash" element={<CrashGame />} />
-          <Route path="/roulette" element={<RouletteGame />} />
-          <Route path="/guess" element={<GuessGame />} />
-          <Route path="/reg" element={<RegisterPage />} />
-          <Route path="/technical" element={<Technical />} />
-          <Route path="*" element={<Waiting />} />
-        </Routes>
-        <ToastContainer />
-      </>
+        <>
+            <Routes>
+                <Route path="/referal" element={<Referal />} />
+                <Route path="/balance" element={<Balance />} />
+                <Route path="/lottery" element={<Lottery />} />
+                <Route path="/game" element={<Game />} />
+                <Route path="/games" element={<GamesPage />} />
+                <Route path="/dice" element={<DicePage />} />
+                <Route path="/dice_game" element={<DiceGame />} />
+                <Route path="/dice_bot" element={<DiceBot />} />
+                <Route path="/blackjack" element={<BlackjackPage />} />
+                <Route path="/blackjack_game" element={<BlackjackGame />} />
+                <Route path="/blackjack_bot" element={<BlackjackBot />} />
+                <Route path="/mines" element={<MinesGame />} />
+                <Route path="/crash" element={<CrashGame />} />
+                <Route path="/roulette" element={<RouletteGame />} />
+                <Route path="/guess" element={<GuessGame />} />
+                <Route path="/reg" element={<RegisterPage />} />
+                <Route path="/technical" element={<Technical />} />
+                <Route path="*" element={<Waiting />} />
+            </Routes>
+            <ToastContainer />
+        </>
     );
-  }
-
-
-
-  return (
-    <>
-      <Routes>
-        <Route path="/referal" element={<Referal />} />
-        <Route path="/balance" element={<Balance />} />
-        <Route path="/lottery" element={<Lottery />} />
-        <Route path="/game" element={<Game />} />
-        <Route path="/games" element={<GamesPage />} />
-        <Route path="/dice" element={<DicePage />} />
-        <Route path="/dice_game" element={<DiceGame />} />
-        <Route path="/dice_bot" element={<DiceBot />} />
-        <Route path="/blackjack" element={<BlackjackPage />} />
-        <Route path="/blackjack_game" element={<BlackjackGame />} />
-        <Route path="/blackjack_bot" element={<BlackjackBot />} />
-        <Route path="/mines" element={<MinesGame />} />
-        <Route path="/crash" element={<CrashGame />} />
-        <Route path="/roulette" element={<RouletteGame />} />
-        <Route path="/guess" element={<GuessGame />} />
-        <Route path="/reg" element={<RegisterPage />} />
-        <Route path="/technical" element={<Technical />} />
-        <Route path="*" element={<Waiting />} />
-      </Routes>
-      <ToastContainer />
-    </>
-  );
 }
 
 export default App;
--- frontend/src/components/Register/RegisterPage.tsx
+++ frontend/src/components/Register/RegisterPage.tsx
@@ -1,26 +1,19 @@
-import {
-    useTonAddress,
-    useTonConnectUI
-} from "@tonconnect/ui-react";
+import { useTonAddress, useTonConnectUI } from "@tonconnect/ui-react";
 import { toast } from "react-toastify";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import getLaunchParams from "../RetrieveLaunchParams";
 import axios from "axios";
 
-
-
 const RegisterPage: React.FC = (): JSX.Element => {
     const [registered, setRegistered] = useState<boolean>(false);
     const tonAddress = useTonAddress(false);
     const [tonConnectUI] = useTonConnectUI();
-    const { initDataRaw, initData } = getLaunchParams();
+    const { initDataRaw } = getLaunchParams();
     const navigate = useNavigate();
 
     tonConnectUI.onStatusChange((wallet) => {
-        if (
-            wallet
-        ) {
+        if (wallet) {
             setRegistered(true);
         }
     });
@@ -30,8 +23,6 @@ const RegisterPage: React.FC = (): JSX.Element => {
             axios
                 .post("/api/player/post", {
                     wallet_address: tonAddress,
-                    telegram_id: initData?.user?.id,
-                    username: initData?.user?.username,
                     initData: initDataRaw,
                 })
                 .then(({ data }) => {
@@ -46,12 +37,11 @@ const RegisterPage: React.FC = (): JSX.Element => {
                 });
         };
         if (registered) fetchRegister();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [registered]);
 
-
     if (tonAddress) {
-        if (!registered)
-            tonConnectUI.disconnect();
+        if (!registered) tonConnectUI.disconnect();
     }
 
     return (
@@ -60,11 +50,16 @@ const RegisterPage: React.FC = (): JSX.Element => {
                 <div className="page-title-cell inter">
                     <b className="page-title-cell-title">Регистрация</b>
                 </div>
-                <div className="page-title-cell inter" style={{ textAlign: "center" }}>
-                    <b className="page-title-cell-title">Условие: </b>на вашем кошельке
-                    должно быть более 5$
+                <div
+                    className="page-title-cell inter"
+                    style={{ textAlign: "center" }}
+                >
+                    <b className="page-title-cell-title">Условие: </b>на вашем
+                    кошельке должно быть более 5$
                     <br />
-                    <b className="page-title-cell-title">или более 30 транзакций</b>
+                    <b className="page-title-cell-title">
+                        или более 30 транзакций
+                    </b>
                 </div>
             </div>
             <div className="page-other">
